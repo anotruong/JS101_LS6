7e1b609f6748e64e920f651026aac233a3f91958
Mel Truong <anotruong@gmail.com>
2021-09-02 20:08:06 +0000

    adding the defense feature was troublesome

--- tictactoe.js
+++ tictactoe.js
@@ -2,6 +2,12 @@ const readline = require('readline-sync');
 const COMP_MARKER = 'O';
 const INITIAL_MARKER = ' ';
 const PLAYER_MARKER = 'X';
+const winCombos = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9],
+    [1, 4, 7], [2, 5, 8], [3, 6, 9],
+    [1, 5, 9], [3, 5, 7]
+];
+
 
 let compScore = 0;
 let playerScore = 0;
@@ -14,7 +20,7 @@ function prompt(msg) {
 function displayBoard(board) {
   console.clear();
 
-  prompt('Your symbol will be X and I will be O.');
+  prompt('You are X and I will be O.');
   console.log('');
   console.log('     |     |');
   console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
@@ -43,13 +49,34 @@ function boardFull(board) {
   return emptySquares(board).length === 0;
 }
 
+function defensiveManuever (line, board) {
+  let markersInLine = line.map(square => board[square]);
+
+  if (markersInLine.filter(mark => mark === PLAYER_MARKER).length === 2) {
+    let unusedSquare = line.find(square => board[square] === INITIAL_MARKER);
+
+    if (unusedSquare !== undefined) {
+      return unusedSquare;
+      }
+  }
+
+  return null;
+}
+
 function comp(board) {
-  let randomIdx = Math.floor(Math.random() * emptySquares(board).length);
+  let square
 
-  let compSquare = emptySquares(board)[randomIdx];
+  for (let idx = 0; idx < winCombos.length; idx++) {
+    let line = winCombos[idx];
+    square = defensiveManuever(line, board);
+    if (square) break;
+  }
 
-  prompt(`I'm going to play ${randomIdx}`);
-  prompt('Your turn!');
+  if (!square) {
+    let randomIdx = Math.floor(Math.random() * emptySquares(board).length);
+    square = emptySquares(board)[randomIdx];
+  }
+
   board[compSquare] = COMP_MARKER;
 }
 
@@ -57,11 +84,24 @@ function emptySquares(board) {
   return Object.keys(board).filter(key => board[key] === INITIAL_MARKER);
 }
 
+function joinOr(arr, punctuation = ', ', conjunction = 'or') {
+  switch (arr.length) {
+    case 0:
+      return ' ';
+    case 1:
+      return `${arr}`;
+    case 2:
+      return `${arr.join(` ${conjunction} `)}`;
+    default:
+      return arr.slice(0, -1).join(punctuation) + `${punctuation}${conjunction} ${arr.slice(-1)}`;
+  }
+}
+
 function player(board) {
   let square;
 
   while (true) {
-    prompt(`Please choose a square ${emptySquares(board).join(', ')}:`);
+    prompt(`Please choose a square ${joinOr(emptySquares(board))}:`);
     square = readline.question().trim();
 
     if (emptySquares(board).includes(square)) break;
@@ -73,16 +113,10 @@ function player(board) {
 }
 
 function someoneWon(board) {
-  return !!detectWinner(board);
+  return detectWinner(board);
 }
 
 function detectWinner(board) {
-  const winCombos = [
-    [1, 2, 3], [4, 5, 6], [7, 8, 9],
-    [1, 4, 7], [2, 5, 8], [3, 6, 9],
-    [1, 5, 9], [3, 5, 7]
-  ];
-
   for (let line = 0; line < winCombos.length; line++) {
     let [ sq1, sq2, sq3 ] = winCombos[line];
 
@@ -104,32 +138,56 @@ function detectWinner(board) {
   return null;
 }
 
-while (true) {
-let board = initializeBoard();
+function pointScorer(board) {
+  if (detectWinner(board) === 'Player') {
+    playerScore += 1;
+    Object.keys(board).map(key => {
+    return board[key] = INITIAL_MARKER;
+    });
+  } else {
+    compScore += 1;
+  }
+}
 
-prompt('Welcome player to the riveting game of TicTacToe!');
+while (true) {
+  let board = initializeBoard();
+
+  prompt('Welcome player to the riveting game of TicTacToe!');
 
   while (true) {
-    displayBoard(board);
 
-    player(board);
-    if (someoneWon(board) || boardFull(board)) break;
+      displayBoard(board);
 
-    comp(board);
-    displayBoard(board);
-    if (someoneWon(board) || boardFull(board)) break;
-  }
+      console.log(playerScore)
 
-  displayBoard(board)
+      player(board);
+      if (someoneWon(board) || boardFull(board)) {
+        pointScorer(board);
+      }
+
+      if (playerScore === 5) break;
+
+      comp(board);
+      displayBoard(board);
+      if (someoneWon(board) || boardFull(board)) {
+        pointScorer(board);
+      }
+
+      if (compScore === 5) break;
+
+    }
+
+  displayBoard(board);
 
   if (someoneWon(board)) {
-    prompt(`${detectWinner(board)} won!`)
+    prompt(`Final score: Player ${playerScore} | Computer ${compScore}`);
+    prompt(`${detectWinner(board)} won!`);
   } else {
-    prompt('It\'s a tie!')
+    prompt('It\'s a tie!');
   }
 
   prompt(`Would you like to play again? y/n`);
-  let answer = readline.question().toLowerCase();
+  let answer = readline.question().toLowerCase()[0];
   if (answer !== 'y') break;
 }
 

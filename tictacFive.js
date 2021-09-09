3a6e56b40574c367d0c0e27ca7aec6c47437771f
Mel Truong <anotruong@gmail.com>
2021-09-09 20:33:36 +0000

    solved most problems

--- tictacFive.js
+++ tictacFive.js
@@ -1,7 +1,11 @@
+//Board Set Up
+
 const readline = require('readline-sync');
 const COMP_MARKER = 'O';
 const INITIAL_MARKER = ' ';
 const HUMAN_MARKER = 'X';
+const MAX_SCORE = 5;
+const yesNo = ['y', 'yes', 'n', 'no', 'r', 'random'];
 const winCombos = [
   [1, 2, 3, 4], [2, 3, 4, 5], [6, 7, 8, 9], [7, 8, 9, 10], [11, 12, 13, 14],
   [12, 13, 14, 15], [16, 17, 18, 19], [17, 18, 19, 20], [21, 22, 23, 24],
@@ -13,10 +17,8 @@ const winCombos = [
   [4, 8, 12, 16], [10, 14, 18, 22] //diagonal
 ];
 
-
 let compScore = 0;
 let humanScore = 0;
-let restart = '';
 
 function prompt(msg) {
   console.log('=>', msg);
@@ -24,29 +26,26 @@ function prompt(msg) {
 
 function displayBoard(board) {
   console.clear();
-
-prompt('You are X and I will be O.');
-console.log('');
-console.log('     |     |     |     |');
-console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}  |  ${board['4']}  |  ${board['5']}`);
-console.log('     |     |     |     |');
-console.log('-----+-----+-----+-----+-----');
-console.log('     |     |     |     |');
-console.log(`  ${board['6']}  |  ${board['7']}  |  ${board['8']}  |  ${board['9']}  |  ${board['10']}`);
-console.log('     |     |     |     |');
-console.log('-----+-----+-----+-----+-----');
-console.log('     |     |     |     |');
-console.log(`  ${board['11']}  |  ${board['12']}  |  ${board['13']}  |  ${board['14']}  |  ${board['15']}`);
-console.log('     |     |     |     |');
-console.log('-----+-----+-----+-----+-----');
-console.log('     |     |     |     |');
-console.log(`  ${board['16']}  |  ${board['17']}  |  ${board['18']}  |  ${board['19']}  |  ${board['20']}`);
-console.log('     |     |     |     |');
-console.log('-----+-----+-----+-----+-----');
-console.log('     |     |     |     |');
-console.log(`  ${board['21']}  |  ${board['22']}  |  ${board['23']}  |  ${board['24']}  |  ${board['25']}`);
-console.log('     |     |     |     |');
-console.log('');
+  console.log('  1     2     3     4     5');
+  console.log('     |     |     |     |');
+  console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}  |  ${board['4']}  |  ${board['5']}`);
+  console.log('     |     |     |     |');
+  console.log('-----+-----+-----+-----+-----');
+  console.log('     |     |     |     |');
+  console.log(`  ${board['6']}  |  ${board['7']}  |  ${board['8']}  |  ${board['9']}  |  ${board['10']}  10`);
+  console.log('     |     |     |     |');
+  console.log('-----+-----+-----+-----+-----');
+  console.log('     |     |     |     |');
+  console.log(`  ${board['11']}  |  ${board['12']}  |  ${board['13']}  |  ${board['14']}  |  ${board['15']}  15`);
+  console.log('     |     |     |     |');
+  console.log('-----+-----+-----+-----+-----');
+  console.log('     |     |     |     |');
+  console.log(`  ${board['16']}  |  ${board['17']}  |  ${board['18']}  |  ${board['19']}  |  ${board['20']}  20`);
+  console.log('     |     |     |     |');
+  console.log('-----+-----+-----+-----+-----');
+  console.log('     |     |     |     |');
+  console.log(`  ${board['21']}  |  ${board['22']}  |  ${board['23']}  |  ${board['24']}  |  ${board['25']}  25`);
+  console.log('     |     |     |     | \n');
 }
 
 function initializeBoard() {
@@ -64,30 +63,35 @@ function boardFull(board) {
 
 function clearBoard(board) {
   Object.keys(board).map(key => {
-    return board[key] = INITIAL_MARKER;
+    board[key] = INITIAL_MARKER;
+    return null;
   });
 }
 
-function findAtRiskSquare (line, board, marker) {
-  let markersInLine = line.map(square => board[square]);
-
-  if (markersInLine.filter(mark => mark === marker).length === 3) {
-  let unusedSquare = line.find(square => board[square] === INITIAL_MARKER);
-
-    if (unusedSquare !== undefined) {
-      return unusedSquare;
-    }
-  }
-  return null;
+function emptySquares(board) {
+  return Object.keys(board).filter(key => board[key] === INITIAL_MARKER);
 }
+
+function scoreBoard() {
+  console.log('|------------- SCORE BOARD -------------|');
+  console.log(`|    Human: ${humanScore}     |     Computer: ${compScore}     |`);
+  console.log('|---------------------------------------|\n');
+}
+
+//Players functions
+
+/*let middleSpaces = (board) => {
+  let importantSpaces = String(Math.floor(Math.random() * (19 - 7 + 1) + 7));
+    if (board[importantSpaces] === INITIAL_MARKER) {
+      board[importantSpaces] === COMP_MARKER;
+    } else {
+      return null;
+    }
+}
+*/
 
 function comp(board) {
   let square;
-
-  if (board[13] === INITIAL_MARKER) {
-    return board[13] = COMP_MARKER;
-  }
-
 
   for (let idx = 0; idx < winCombos.length; idx++) {
     let line = winCombos[idx];
@@ -96,11 +100,11 @@ function comp(board) {
   }
 
   if (!square) {
-  for (let idx = 0; idx < winCombos.length; idx++) {
-    let line = winCombos[idx];
-    square = findAtRiskSquare(line, board, HUMAN_MARKER);
-    if (square) break;
-   }
+    for (let idx = 0; idx < winCombos.length; idx++) {
+      let line = winCombos[idx];
+      square = findAtRiskSquare(line, board, HUMAN_MARKER);
+      if (square) break;
+    }
   }
 
   if (!square) {
@@ -109,23 +113,6 @@ function comp(board) {
   }
 
   board[square] = COMP_MARKER;
-}
-
-function emptySquares(board) {
-  return Object.keys(board).filter(key => board[key] === INITIAL_MARKER);
-}
-
-function joinOr(arr, punctuation = ', ', conjunction = 'or') {
-  switch (arr.length) {
-    case 0:
-      return ' ';
-    case 1:
-      return `${arr}`;
-    case 2:
-      return `${arr.join(` ${conjunction} `)}`;
-    default:
-      return arr.slice(0, -1).join(punctuation) + `${punctuation}${conjunction} ${arr.slice(-1)}`;
-  }
 }
 
 function human(board) {
@@ -141,35 +128,10 @@ function human(board) {
   }
 
   board[square] = HUMAN_MARKER;
-}
-
-function someoneWon(board) {
-  return detectWinner(board);
-}
-
-function detectWinner(board) {
-  for (let line = 0; line < winCombos.length; line++) {
-    let [ sq1, sq2, sq3, sq4 ] = winCombos[line];
-
-    if (
-      board[sq1] === HUMAN_MARKER &&
-        board[sq2] === HUMAN_MARKER &&
-        board[sq3] === HUMAN_MARKER &&
-        board[sq4] === HUMAN_MARKER
-    ) {
-      return 'Player';
-    } else if (
-      board[sq1] === COMP_MARKER &&
-        board[sq2] === COMP_MARKER &&
-        board[sq3] === COMP_MARKER &&
-        board[sq4] === COMP_MARKER
-    ) {
-      return 'Computer';
-    }
-  }
-
   return null;
 }
+
+//Game Functions
 
 function alternatePlayer(player) {
   if (player === 'human') {
@@ -182,8 +144,70 @@ function alternatePlayer(player) {
 function chooseSquare(board, currentPlayer) {
   if (currentPlayer === 'human') {
     return human(board);
-  } else if (currentPlayer === 'comp') {
+  } else {
     return comp(board);
+  }
+  return null;
+}
+
+function detectWinner(board) {
+  for (let line = 0; line < winCombos.length; line++) {
+    let [ sq1, sq2, sq3, sq4 ] = winCombos[line];
+    if (
+      board[sq1] === HUMAN_MARKER &&
+       board[sq2] === HUMAN_MARKER &&
+            board[sq3] === HUMAN_MARKER &&
+            board[sq4] === HUMAN_MARKER
+    ) {
+      return 'human';
+    } else if (
+      board[sq1] === COMP_MARKER &&
+            board[sq2] === COMP_MARKER &&
+            board[sq3] === COMP_MARKER &&
+            board[sq4] === COMP_MARKER
+    ) {
+      return 'comp';
+    }
+  }
+  return null;
+}
+
+function enterToContinue() {
+  prompt("Let me know when you're ready to play again.\n");
+  prompt("Press 'Enter' to continue.");
+
+  let userInput = readline.question().toLowerCase();
+
+  while (userInput === false) {
+    prompt("Meep, something went wrong. You can press 'Enter' or any key to continue");
+    userInput = readline.question().toLowerCase();
+  }
+}
+
+
+function findAtRiskSquare (line, board, marker) {
+  let markersInLine = line.map(square => board[square]);
+
+  if (markersInLine.filter(mark => mark === marker).length === 3) {
+    let unusedSquare = line.find(square => board[square] === INITIAL_MARKER);
+
+    if (unusedSquare !== undefined) {
+      return unusedSquare;
+    }
+  }
+  return null;
+}
+
+function joinOr(arr, punctuation = ', ', conjunction = 'or') {
+  switch (arr.length) {
+    case 0:
+      return ' ';
+    case 1:
+      return `${arr}`;
+    case 2:
+      return `${arr.join(` ${conjunction} `)}`;
+    default:
+      return arr.slice(0, -1).join(punctuation) + `${punctuation}${conjunction} ${arr.slice(-1)}`;
   }
 }
 
@@ -193,12 +217,13 @@ function pointScorer(board) {
   } else {
     compScore += 1;
   }
-  prompt(`Looks like ${detectWinner(board)} won this round, ready for the next round? yes or yes?`)
-  let answer = readline.question().toLowerCase();
+  scoreBoard();
+  enterToContinue();
+  clearBoard(board);
+}
 
-  if (answer === 'yes') {
-    clearBoard(board);
-  }
+function someoneWon(board) {
+  return detectWinner(board);
 }
 
 function winOrFull(board) {
@@ -207,29 +232,23 @@ function winOrFull(board) {
     pointScorer(board);
   } else if (boardFull(board)) {
     displayBoard(board);
-    prompt("Looks like the board is full, ready for the next round? Type 'yes'.")
-    let answer = readline.question().toLowerCase()[0];
-
-    while (!(answer.includes('y'))) {
-      prompt("That's not the proper answer. Let me know when you are ready for the next round. Type 'yes'.");
-    };
-
-    if (answer === 'y') {
-    clearBoard(board);
-    }
   }
 }
+
+let random = () => Math.floor(Math.random() * 2);
+
 
 while (true) {
   let board = initializeBoard();
 
-  prompt('Welcome player to the riveting game of TicTacToe!');
+  prompt('Welcome human to the riveting game of TicTacToe!');
+  prompt("It's easy to play, but not easy to win.");
+  prompt("We are playing the long game, whoever gets 5 points first wins!\n");
 
-  prompt("Would you like to go first? y/n")
-
+  prompt("Would you like to go first or randomize it? yes/no/random");
   let currentPlayer = readline.question().toLowerCase()[0];
 
-  while (!((currentPlayer.includes('y')) || (!currentPlayer.includes('n')))) {
+  while (!yesNo.includes(currentPlayer)) {
     prompt("That's not a valid choice");
     currentPlayer = readline.question().toLowerCase()[0];
   }
@@ -241,43 +260,54 @@ while (true) {
     case 'n':
       currentPlayer = comp;
       break;
+    case 'r':
+      if (random() === 0) {
+        currentPlayer = comp;
+      } else {
+        currentPlayer = human;
+      }
+      break;
   }
 
-  while (true) {
+  do {
 
-    displayBoard(board);
+  displayBoard(board);
 
-    prompt(`Current score: Human ${humanScore} | Computer ${compScore}`);
+  scoreBoard();
 
+
+    prompt('You are X and I am O');
     chooseSquare(board, currentPlayer);
     currentPlayer = alternatePlayer(currentPlayer);
     winOrFull(board);
+    console.clear();
 
-    if (humanScore === 5) break;
-
-    if (compScore === 5) break;
-  }
-
+  } while ((humanScore || compScore) < MAX_SCORE);
   displayBoard(board);
-  prompt(`Final score: Human ${humanScore} | Computer ${compScore}`);
 
-  if (compScore === 5) {
+  scoreBoard();
+
+  if (compScore === MAX_SCORE) {
     prompt(`Computer wins!`);
-  } else if (playerScore === 5) {
+  } else if (humanScore === MAX_SCORE) {
     prompt('Human wins!');
   } else {
     prompt('It\'s a tie!');
   }
 
-  prompt(`Would you like to play again? y/n`);
-  let answer = readline.question().toLowerCase()[0];
+  prompt(`Would you like to play again? yes/no`);
+  let answer = readline.question().toLowerCase();
 
-  while (!((answer.includes('y') || (answer.includes('n')))) {
-    prompt('That answer is invalid. Would you like to play again or exit the game? y/n');
-    answer = readline.question().toLowerCase()[0];
+  while (!yesNo.includes(answer)) {
+    prompt("That answer is invalid. Would you like to play again or exit the game? y/n");
+    answer = readline.question().toLowerCase();
   }
 
   if (answer !== 'y') break;
+
+  compScore = 0;
+  humanScore = 0;
+  console.clear();
 }
 
 prompt('Thanks for playing Tic Tac Toe~');

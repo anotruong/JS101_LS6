55776b7a228796e71e832cd02c4c7e261d286895
Mel Truong <anotruong@gmail.com>
2021-09-20 23:04:01 +0000

    finished the game without scoreboard

--- twentyone.js
+++ twentyone.js
@@ -1,6 +1,6 @@
 //Game play//
 const readline = require('readline-sync');
-const emptyDisplay = '            ';
+const EMPTY_DISPLAY = '            ';
 const FIFTY_TWO_CARDS = [
   ['C', 'A'], ['D', 'A'], ['H', 'A'], ['S', 'A'],
   ['C', '2'], ['D', '2'], ['H', '2'], ['S', '2'],
@@ -16,17 +16,22 @@ const FIFTY_TWO_CARDS = [
   ['C', 'Q'], ['D', 'Q'], ['H', 'Q'], ['S', 'Q'],
   ['C', 'K'], ['D', 'K'], ['H', 'K'], ['S', 'K']
 ];
+const HIT_STAY = ['hit', 'stay'];
 const MAX_SCORE = 21;
+const NO_YES = ['yes', 'no'];
 
 let dealersHand = [];
 let hiddenScore = 0;
 let playersHand = [];
 
+//DISPLAY BOARD INFO//
+
 function displayBoard(board) {
   console.clear();
 
   console.log('|              |              |');
-  console.log(`|  PLAYER: ${sumOfHand(playersHand)}  |  DEALER: ${dealerScoreSpacing(hiddenScore)}  |`);
+//  console.log(`|  PLAYER: ${sumOfHand(playersHand).length}  |  DEALER: ${kerning(hiddenScore)}  |`);
+  console.log(`|  PLAYER: ${kerning(sumOfHand(playersHand))}  |  DEALER: ${kerning(hiddenScore)}  |`);
   console.log('|--------------|--------------|');
   console.log(`| ${board['0']} | ${board['1']} |`);
   console.log(`| ${board['2']} | ${board['3']} |`);
@@ -37,11 +42,11 @@ function displayBoard(board) {
   console.log('|--------------|--------------|');
 }
 
-function dealerScoreSpacing(hiddenScore) {
-  if (hiddenScore.length === 1) {
-    return hiddenScore + ' ';
+function kerning(score) {
+  if (String(score).length === 2) {
+    return `${score}`;
   } else {
-   return hiddenScore;
+   return score + ' ';
   }
 }
 
@@ -61,10 +66,48 @@ function initializeBoard() {
   let obj = {};
 
   for (let idx = 0; idx <= 11; idx++) {
-    obj[String(idx)] = emptyDisplay;
+    obj[String(idx)] = EMPTY_DISPLAY;
   }
 
   return obj;
+}
+
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
+
+//GAMEPLAY FUNCTIONS//
+
+function busted() {
+  if (sumOfHand(playersHand) >= MAX_SCORE) {
+      return true;
+    } else if (sumOfHand(dealersHand) >= MAX_SCORE) {
+      return true;
+    }
+}
+
+function dealersHiddenCard(ele) {
+  let sum = 0;
+
+  if (['J', 'Q', 'K']. includes(ele)) {
+    sum += 10;
+  } else if (ele === 'A') {
+    sum += 11;
+  } else {
+    sum += Number(ele);
+  }
+  return sum;
+}
+
+function enterToContinue() {
+  prompt("Press 'Enter' to continue.");
+
+  let userInput = readline.question().toLowerCase();
+
+  while (userInput === false) {
+    prompt("Meep, something went wrong. You can press 'Enter' or any key to continue");
+    userInput = readline.question().toLowerCase();
+  };
 }
 
 function sumOfHand(player) {
@@ -100,7 +143,13 @@ function shuffle(FIFTY_TWO_CARDS) {
 //calculating the result.
 
 function whoWon() {
+  if (sumOfHand(dealersHand) < sumOfHand(playersHand) <= MAX_SCORE) {
+    prompt('Player Wins!');
+  } else if (sumOfHand(playerHand) < sumOfHand(dealersHand)<=  MAX_SCORE) {
+    prompt('Dealer Wins!');
+  }
 
+  return null;
 }
 
 //Create a function that only handles displaying the result.
@@ -110,16 +159,27 @@ function displayWinner() {
 
 }
 
-//Player functions//
+//PLAYER FUNCTIONS//
 
-function dealerMoves() {
-  let sum = sumOfHand(dealerHand);
+function dealersTurn() {
+  let sum;
+  let arrIdx = 2;
+  let boardIdx = 5;
 
-  while (sum <= 17) {
+  board['1'] = displayCard(dealersHand[0]);
+
+ do {
+    displayBoard(board);
+
     dealersHand.push(shuffle(FIFTY_TWO_CARDS)[0]);
-    sum += sumOfHand(dealersHand);
 
-  }
+    board[String(boardIdx)] = displayCard(dealersHand[arrIdx]);
+
+    sum = sumOfHand(dealersHand);
+
+    arrIdx++;
+    boardIdx += 2;
+  } while (sum <= 17);
 
   hiddenScore = sum;
 
@@ -127,22 +187,20 @@ function dealerMoves() {
 }
 
 function firstTwoTurns() {
+  //first drawl
   playersHand.push(shuffle(FIFTY_TWO_CARDS)[0]);
   board['0'] = displayCard(playersHand[0]);
 
   dealersHand.push(shuffle(FIFTY_TWO_CARDS)[0]);
 
-  hiddenScore = sumOfHand(dealersHand) - Number(dealersHand[0][1]);
-
-  displayBoard(board);
-
+  //second drawl
   playersHand.push(shuffle(FIFTY_TWO_CARDS)[0]);
   board['2'] = displayCard(playersHand[1]);
 
   dealersHand.push(shuffle(FIFTY_TWO_CARDS)[0]);
   board['3'] = displayCard(dealersHand[1]);
 
-  hiddenScore = sumOfHand(dealersHand) - Number(dealersHand[0][1]);
+  hiddenScore = sumOfHand(dealersHand) - dealersHiddenCard(dealersHand[0][1]);
 }
 
 function playersTurn() {
@@ -150,34 +208,74 @@ function playersTurn() {
   let handIdx = 2;
   let objIdx = 4;
 
-  while (true) {
+ while (sum <= MAX_SCORE) {
+
     displayBoard(board);
 
-    console.log("hit or stay?");
+    prompt("Do you want to hit or stay?");
     let answer = readline.question().toLowerCase();
 
-    if (answer === 'stay') break;
+    while (!HIT_STAY.includes(answer)) {
+      prompt("That's not a valid answer. Do you want to hit or stay?");
+      answer = readline.question().toLowerCase();
+    }
+
+    if (answer !== 'hit') break;
 
     playersHand.push(shuffle(FIFTY_TWO_CARDS)[0]);
-
     board[String(objIdx)] = displayCard(playersHand[handIdx]);
-
     handIdx++;
     objIdx += 2;
+  };
 
-//  if (busted()) {
-  // prob end the game? or ask the user to play again?
-//  } else {
- //   console.log("You chose to stay!");
-  }
+  return null;
 }
 
 let board = initializeBoard();
 
 while (true) {
+
   displayBoard(board);
-//intro to the game
-//ask them if they are ready to start;
-  firstTwoTurns();
-  playersTurn();
+
+  prompt('Welcome to the Simply Twenty One.\n');
+  prompt("It's similar to Black Jack, except it's simple.");
+
+  enterToContinue();
+
+  do {
+
+    displayBoard(board);
+
+    firstTwoTurns();
+
+    playersTurn();
+    displayBoard(board);
+
+    if (busted()) break;
+
+    dealersTurn();
+    displayBoard(board);
+
+    if (busted()) break;
+
+} while ((sumOfHand(playersHand) || sumOfHand(dealersHand)) <= MAX_SCORE);
+
+whoWon();
+
+prompt("Did you want to play again? Yes/No?");
+let restart = readline.question().toLowerCase();
+
+while (!NO_YES.includes(restart)) {
+  prompt("That wasn't a valid answer. Yes or No?")
+  restart = readline.question().toLowerCase();
 }
+
+if (restart === 'no') break;
+
+dealersHand = [];
+playersHand = [];
+
+console.clear();
+}
+
+prompt("Thanks for playing!");
